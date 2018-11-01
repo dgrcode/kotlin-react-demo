3a7537cce1544760711cff5a46c9f9faa52c1af3
Daniel Gonzalez Reina <dangonrei@gmail.com>
2018-11-01 21:45:38 +0000

    1. Update without state

--- src/app/App.kt
+++ src/app/App.kt
@@ -1,11 +1,34 @@
 package app
 
+import kotlin.js.*
+import kotlinx.html.js.onClickFunction
+import org.w3c.dom.events.Event
 import react.*
 import react.dom.*
 
 class App : RComponent<RProps, RState>() {
+    var count = 0
+
+    fun increment (evt: Event) {
+        count++
+        console.log("count: $count")
+    }
+
+    fun decrement (evt: Event) {
+        count--
+        console.log("count: $count")
+    }
+
     override fun RBuilder.render() {
-        +"App"
+        +"${count}"
+        button () {
+            attrs.onClickFunction = ::increment
+            +"Add"
+        }
+        button () {
+            attrs.onClickFunction = ::decrement
+            +"Remove"
+        }
     }
 }
 
